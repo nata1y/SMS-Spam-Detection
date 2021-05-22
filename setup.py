ab7b0c489017cee9be24ba09e6c6d27021a5bcfc
Natalia <oxi-flent@mail.ru>
2021-05-22 23:04:47 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='SMS-Spam-Detection',
+      version='0.1',
+      description="Spam detection tool",
+      long_description="",
+      author="",
+      author_email="",
+      install_requires=[],
+      packages=find_packages(),
+)
