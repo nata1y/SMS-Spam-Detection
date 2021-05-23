c9bd60f6744d2ed54de1b17594bc6b7b07223a37
Natalia <oxi-flent@mail.ru>
2021-05-23 10:01:29 +0200

    add conf and index.rst

---
+++ doc/source/conf.py
@@ -0,0 +1,37 @@
+
+# -- Project information -----------------------------------------------------
+
+project = 'remla-spam-detect'
+copyright = '2019, AUTHOR'
+author = 'AUTHOR'
+
+# The short X.Y version
+version = '0.1'
+
+# The full version, including alpha/beta/rc tags
+release = '0.1'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = []
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['templates']
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
