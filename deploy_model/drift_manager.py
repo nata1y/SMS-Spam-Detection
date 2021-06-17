e047cacff3e351f463de0df675aa4f49feed73ed
Job Kanis <jobkanis@gmail.com>
2021-06-17 11:32:10 +0200

    Update drift_manager and merge with master

--- deploy_model/drift_manager.py
+++ deploy_model/drift_manager.py
@@ -7,6 +7,7 @@ from datadrift_detect.detect_alibi import _detect_drift
 from deploy_model.feed_data_artificially import get_all_stats
 from deploy_model.util import load_best_clf
 
+
 class DriftManager:
     window_size = 500
     metricsManager: MetricsManager
@@ -20,7 +21,6 @@ class DriftManager:
 
     def __init__(self, metricsManager: MetricsManager) -> None:
         self.metricsManager = metricsManager
-        self.initializeMetrics()
 
         self.name = "Manager 1"
         self.calls = 0
@@ -47,23 +47,6 @@ class DriftManager:
     def add_real_label(self, real_label):
         self.real_labels.append(real_label)
 
-    def initializeMetrics(self):
-        # metric: driftdetection_nlp_results
-        self.metricsManager.newMetric("driftdetection_nlp_results",
-                                      "Drift Detection results of the NLP-model",
-                                      0)
-        # metric: driftdetection_loss_results
-        self.metricsManager.newMetric("driftdetection_loss_results",
-                                      "Drift Detection results of the loss model",
-                                      0)
-        # metric: driftdetection_regression_results
-        self.metricsManager.newMetric("driftdetection_regression_results",
-                                      "Drift Detection results of the regression model",
-                                      0)
-        self.metricsManager.newMetric("driftdetection_test_metric",
-                                      "Drift Detection test metric",
-                                      0)
-
     def add_call(self, prediction, drift_type):
         self.drift_type = drift_type
         self.calls = self.calls + 1
@@ -79,65 +62,49 @@ class DriftManager:
             if len(self.data) % self.window_size == 0:
                 self.calculate_drifts()
 
-    driftdetect_test_metric: int = 0
     def calculate_drifts(self):
-        self.driftdetect_test_metric += 2
-        self.metricsManager.updateMetric("driftdetection_test_metric", self.driftdetect_test_metric)
-
         analysis_csv_row = f"{self.drift_type},"
 
         print("Checking complete incoming dataset for data drift...")
+
         full_set = pd.DataFrame(np.array(self.data[-self.window_size:]), columns=['label', 'message'])
         for detection in _detect_drift(full_set, self.preprocessed[-self.window_size:]):
             name = detection['meta']['name']
             drift_bool = detection['data']['is_drift']
-
             analysis_csv_row += f"{drift_bool},"
-
             dist = detection['data']['distance']
             if type(dist) is np.ndarray:
-                for x in dist:
-                    analysis_csv_row += f"{x}," 
+                for i, v in enumerate(dist):
+                    analysis_csv_row += f"{v},"
+                    self.metricsManager.updateMetric("driftdetection_{}_result_{}".format(name, i), v)
             else:
-                analysis_csv_row += f"{dist}," 
- 
+                analysis_csv_row += f"{dist},"
+                self.metricsManager.updateMetric("driftdetection_{}_result", dist)
 
-
-
-        # print("Check for concept drift using NLP and loss distribution")
         nlp_stats, loss_stats, regression_stats = get_all_stats(
             self.real_labels[-self.window_size:],
             full_set, self.clf, self.window_size)
 
         # METRIC: driftdetection_nlp_results
-        nlp_results = str(nlp_stats.iloc[-1:])
-        self.metricsManager.updateMetric("driftdetection_nlp_results", nlp_results)
-        print("NLP Results:\n " + nlp_results)
-
         value = nlp_stats['kl_divergence'].tolist()[-1]
         drift_bool = nlp_stats['kl_divergence'].tolist()[-1] > self.thresholds['nlp']
 
+        self.metricsManager.updateMetric("driftdetection_nlp_results", value)
         analysis_csv_row += f"{value},{(1 if drift_bool else 0)},"
 
         # METRIC: driftdetection_loss_results
-        loss_results = str(loss_stats.iloc[-1:])
-        self.metricsManager.updateMetric("driftdetection_loss_results", loss_results)
-        print("Loss Results:\n" + loss_results)
-
         value = loss_stats['loss_dist'].tolist()[-1]
         drift_bool = loss_stats['loss_dist'].tolist()[-1] > self.thresholds['loss']
 
         analysis_csv_row += f"{value},{(1 if drift_bool else 0)},"
+        self.metricsManager.updateMetric("driftdetection_loss_results", value)
 
         # METRIC: driftdetection_regression_results
-        regression_results = str(regression_stats.iloc[-1:])
-        self.metricsManager.updateMetric("driftdetection_regression_results", regression_results)
-        print("Regression Results:\n" + regression_results)
-
         value = regression_stats['predicted_performance'].tolist()[-1]
         drift_bool = regression_stats['predicted_performance'].tolist()[-1] < self.thresholds['regression']
 
         analysis_csv_row += f"{value},{(1 if drift_bool else 0)}"
+        self.metricsManager.updateMetric("driftdetection_regression_results", value)
 
         f = open("output/combined_stats.csv", "a")
         f.write(analysis_csv_row + "\n")
