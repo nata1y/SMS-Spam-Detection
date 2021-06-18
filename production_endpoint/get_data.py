f976d8b225a6ad9d4a1ac263c3a7335d96cfcbc1
Rover van der Noort <s.r.vandernoort@student.tudelft.nl>
2021-06-18 11:56:11 +0200

    Fix pylint and remove main function

--- production_endpoint/get_data.py
+++ production_endpoint/get_data.py
@@ -4,26 +4,28 @@ Download and extract data.
 import urllib.request
 import zipfile
 
-from deploy_model.util import remove_file
+from deploy_model.util import remove_file, ensure_path_exists
+from train_model.util import REGRESSION_DATA_DIR, DATASET_DIR
 
 # URL = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip'
 # URL = 'https://surfdrive.surf.nl/files/index.php/s/OZRd9BcxhGkxTuy/download' # V2
 # URL = 'https://surfdrive.surf.nl/files/index.php/s/H4e35DvjaX18pTI/download' # V3
 URL = 'https://surfdrive.surf.nl/files/index.php/s/HU5mY29RzxRlHCU/download' # V4
-DIFF_DIR = "dataset"
-EXTRACT_DIR = "dataset/regression"
+
+ensure_path_exists(DATASET_DIR)
+ensure_path_exists(REGRESSION_DATA_DIR)
 
 def get_data():
     '''Get the production data.'''
     zip_path, _ = urllib.request.urlretrieve(URL)
     with zipfile.ZipFile(zip_path, "r") as file:
-        file.extractall(EXTRACT_DIR)
+        file.extractall(REGRESSION_DATA_DIR)
 
-    remove_file(EXTRACT_DIR + '/SMSSpamCollection_diff')
+    remove_file(REGRESSION_DATA_DIR + '/SMSSpamCollection_diff')
 
-    with open(EXTRACT_DIR + '/SMSSpamCollection', 'r') as src, \
-        open(DIFF_DIR + '/SMSSpamCollection', 'r') as diff, \
-        open(EXTRACT_DIR + '/SMSSpamCollection_diff', 'a') as dest:
+    with open(REGRESSION_DATA_DIR + '/SMSSpamCollection', 'r') as src, \
+        open(DATASET_DIR + '/SMSSpamCollection', 'r') as diff, \
+        open(REGRESSION_DATA_DIR + '/SMSSpamCollection_diff', 'a') as dest:
         nonempty_lines = [line.strip("\n") for line in diff if line != "\n"]
         ignore = len(nonempty_lines)
         i = 0
--- train_model/get_data.py
+++ train_model/get_data.py
@@ -1,20 +1,15 @@
-'''Download and extract data.'''
-from deploy_model.util import ensure_path_exists
+'''Download and extract training data.'''
 import zipfile
 import urllib.request
 
 from train_model.util import DATASET_DIR
+from deploy_model.util import ensure_path_exists
 
 # URL = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip'
 URL = 'https://surfdrive.surf.nl/files/index.php/s/WCPP8WJPrtCbUO5/download'
 
 ensure_path_exists(DATASET_DIR)
 
-def main():
-    '''Extract url contents to dataset directory.'''
-    zip_path, _ = urllib.request.urlretrieve(URL)
-    with zipfile.ZipFile(zip_path, "r") as file:
-        file.extractall(DATASET_DIR)
-
-if __name__ == 'main':
-    main()
+zip_path, _ = urllib.request.urlretrieve(URL)
+with zipfile.ZipFile(zip_path, "r") as file:
+    file.extractall(DATASET_DIR)
